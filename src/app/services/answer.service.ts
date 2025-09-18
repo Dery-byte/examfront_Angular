7704fbd9426807c9d7c2bce3a7e5f431c51eae1e
dery-byte <emmanueldery31@gmail.com>
2025-09-18 07:46:05 +0000

    theory evaluation works2

---
+++ src/app/services/answer.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import baseUrl from './helper';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnswerService {
+constructor( private _http:HttpClient ) { }
+
+
+
+
+
+  //get Report by userId and QuizId
+  // get Report by quizId
+public getTheoryReport(qId: number): Observable<any> {
+  const token = localStorage.getItem('token'); // or however you store it
+  const headers = {
+    Authorization: `Bearer ${token}`
+  };
+  return this._http.get(`${baseUrl}/answers/quiz/${qId}`, { headers });
+}
+
+// public getTheoryReport(qId){
+//   return this._http.get(`${baseUrl}//answers/quiz/${qId}`);
+// }
+
+}
